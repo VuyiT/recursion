bd95a5a87077ff68f05d4c65b68601cda57589e4
Avuyile Tiki <avuyiletiki14@gmail.com>
2025-08-25 09:59:48 +0200

    Create recursive fib

--- fib.js
+++ fib.js
@@ -7,3 +7,22 @@ function fibs(n) {
     return array;
 }
 console.log(fibs(9));
+
+function fibRecs(n) {
+    if (n <= 0) {
+        return [];
+    }
+    if (n === 1) {
+        return [0]
+    }
+    if (n === 2) {
+        return [0, 1];
+    }
+
+    if (n > 2) {
+        let array = fibRecs(n - 1);
+        array.push(array[array.length - 1] + array[array.length - 2]);
+        return array;
+    }
+}
+console.log(fibRecs(9));
